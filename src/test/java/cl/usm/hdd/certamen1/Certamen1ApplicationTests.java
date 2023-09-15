10b155f32621c57c47363d1287bf47f1696e8e6c
unknown <diejamudelp@hotmail.com>
2023-09-15 14:45:49 -0300

    init

---
+++ src/test/java/cl/usm/hdd/certamen1/Certamen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.certamen1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Certamen1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
